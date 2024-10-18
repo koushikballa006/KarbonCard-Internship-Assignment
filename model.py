142736466d5aaeabda911a47969ce6920dee7889
koushikballa006 <koushikballa06@gmail.com>
2024-10-18 19:13:00 +0530

    analyze_data function

--- model.py
+++ model.py
@@ -53,10 +53,4 @@ def analyze_data():
         return jsonify({"error": "Invalid file type. Please upload a JSON file."}), 400        
 
 if __name__ == "__main__":
-    # data = json.loads("t.json")
-    # print(data)
-    with open("data.json", "r") as file:
-        content = file.read()
-        # convert to json
-        data = json.loads(content)
-        print(probe_model_5l_profit(data["data"]))
+    app.run(debug=True)
